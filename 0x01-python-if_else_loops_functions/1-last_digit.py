0305cbb57f32a79a9f1a0e0ecaeb28d1445385e6
Amir <amirmurad696@gmail.com>
2024-01-03 22:15:07 +0300

    Changed small letter words to capital because the checker is failing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number > 0:
 if number < 0:
     ld = (int(str(number)[-1]) * -1)
 if ld == 0:
-    print(f"last digit of {number} is 0 and is 0")
+    print(f"Last digit of {number} is 0 and is 0")
 if ld > 5:
-    print(f"last digit of {number} is {ld} and is greater than 5")
+    print(f"Last digit of {number} is {ld} and is greater than 5")
 if ld < 6 and ld != 0:
-    print(f"last digit of {number} is {ld} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
