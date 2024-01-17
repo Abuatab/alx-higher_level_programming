950d7fcbca869408f8858bc589d6657f60c9f6e3
Amir <amirmurad696@gmail.com>
2024-01-17 19:18:13 +0300

    changed it to a 3-d array

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     squared = []
-    if len(matrix) == 0:
-        return squared
     for i in matrix:
+        row = []
         for j in i:
-            squared.append(j * j)
-    return(squared)
+            row.append(j * j)
+            squared.append(row)
+        return(squared)
