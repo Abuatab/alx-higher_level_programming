7d236cd4acdad4aaa4d2dde719c49e66a4323c8a
Amir <amirmurad696@gmail.com>
2024-01-17 19:20:39 +0300

    minor fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     squared = []
+    if len(matrix) == 0:
+        return squared
     for i in matrix:
         row = []
         for j in i:
