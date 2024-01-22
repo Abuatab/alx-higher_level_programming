1f6a04921c67dad8d135a200ab6504f0fd026b0d
Amir <amirmurad696@gmail.com>
2024-01-22 22:47:37 +0300

    Removed the IndexError exeption because the requirements states that it's expected to happen

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num_of_elements += 1
-        except IndexError:
-            break
         except (ValueError, TypeError):
             continue
     print("")
