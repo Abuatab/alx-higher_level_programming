5383e7e9d9695b0ba0a6f51006889833403964b3
Amir <amirmurad696@gmail.com>
2024-01-22 22:43:11 +0300

    Done with the 3rd task

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_of_elements = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num_of_elements += 1
+        except IndexError:
+            break
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return num_of_elements
