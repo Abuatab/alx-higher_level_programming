7cc3805cb46f6b483dd84dd93294c7b10967dad3
Amir <amirmurad696@gmail.com>
2024-01-30 17:42:14 +0300

    Another indentation error fixed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,7 +9,7 @@ class Square:
         self.size = size
 
     def area(self):
-            return self.__size * self.__size
+        return self.__size * self.__size
 
     @property
     def size(self):
