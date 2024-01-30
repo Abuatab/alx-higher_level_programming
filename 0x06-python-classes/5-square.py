31d10d28bce57e953b6e8c9125c4401b1512b2db
Amir <amirmurad696@gmail.com>
2024-01-30 20:32:29 +0300

    isinstance instead of type

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -30,7 +30,7 @@ class Square:
     @size.setter
     def size(self, value):
         """ Sets the size of the square to the given value."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
