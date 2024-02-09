9ce69bd0e7ccc4029db241dd2e6af14d4764504c
Amir <amirmurad696@gmail.com>
2024-02-09 10:50:55 +0300

    Done with the 7th task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def all_combs():
+    for i in range(10):
+        for j in range(10):
+            if i == j or i > j:
+                continue
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j))
+                break
+            print("{}{}".format(i, j), end=", ")
