3153b818311a3e98162322470d87d7ab250e662e
Amir <amirmurad696@gmail.com>
2024-02-10 17:21:47 +0300

    handled if i is an upper case letter already

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        print("{}".format(chr(ord(i) - 32)), end="")
+        if ord(i) > 96 and ord(i) < 123:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
     print()
