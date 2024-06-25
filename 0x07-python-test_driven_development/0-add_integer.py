b9fe79c29b41ef2ca96ebe0253ec6408e4c89db2
Amir Murad <amirmurad696@gmail.com>
2024-06-25 16:44:37 +0300

    A fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-    Defines a function that adds two numbers
-"""
+"""Defines a function that adds two numbers"""
 
 
 def add_integer(a, b=98):
@@ -18,3 +16,8 @@ def add_integer(a, b=98):
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
