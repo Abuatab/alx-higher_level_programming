035f131c0729cf3fdcedaa9871a04c8d10848fc4
Amir Murad <amirmurad696@gmail.com>
2024-07-29 13:32:36 +0300

    Added doc strings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 
+"""Defines a class Rectangle."""
+
 class Rectangle:
+    """A rectangle representation"""
+
     def __init__(self, width=0, height=0):
+        """Initializes a new rectangle.
+        Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Get/set the width of the rectangle."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
