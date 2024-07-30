59b54e0c7706873675936b5bd405f16205c81358
Amir Murad <amirmurad696@gmail.com>
2024-07-30 18:14:46 +0300

    Returned to the previous version

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -70,4 +70,4 @@ class Rectangle:
     def __repr__(self):
         """ returns a string representation of the rectangle
         to be able to recreate a new instance by using eval()"""
-        return "(Rectangle({}, {}))".format(str(self.__width), str(self.__height))
+        return "(Rectangle({}, {}))".format(self.__width, self.__height)
