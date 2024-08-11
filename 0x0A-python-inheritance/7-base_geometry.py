901a2a5c344cd95d71336a55cdc6a354184940b6
Amir Murad <amirmurad696@gmail.com>
2024-08-11 12:43:30 +0300

    Changed != to is not

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,14 +11,15 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
+
         Args:
-        name (str): The name of the parameter.
-        value (int): The parameter to validate.
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
         Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is <= 0.
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
