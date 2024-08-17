c7acca5c1929bd40fdaf3f5b6a14a431c14803f3
Amir Murad <amirmurad696@gmail.com>
2024-08-17 17:38:38 +0300

    Removed the function call

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,8 +5,4 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding='utf-8') as f:
-        f_contents = f.read()
-        print(f_contents)
-
-
-read_file('my_file_0.txt')
+        print(f.read())
