2429cbdc52209a0eb44bb64a750ddfb4bc11ac49
Amir Murad <amirmurad696@gmail.com>
2024-08-17 19:46:17 +0300

    Wrong function name

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Defines a function that appends to a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """appends a string at the end of a text file (UTF8) and returns
     the number of characters added"""
     with open(filename, "a") as f:
