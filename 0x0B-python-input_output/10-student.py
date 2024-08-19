aa3da3796a0bcef8b83fcaafabfbc445bda65b42
Amir Murad <amirmurad696@gmail.com>
2024-08-19 17:01:43 +0300

    Accidentaly pushed with the main file inside it

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,15 +18,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
-
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
-
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
-
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
+            return {key: value for key, value in self.__dict__.items(
+            ) if key in attrs}
