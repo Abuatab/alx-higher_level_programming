4d78d3fb279ecc38ce53e66b2ff752e8e7af7722
Amir Murad <amirmurad696@gmail.com>
2024-08-19 16:42:22 +0300

    Done with the eleventh task

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,4 +14,9 @@ class Student:
     def to_json(self):
         """Retrieves a dictionary representation of a Student instan
         ce"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items(
+            ) if key in attrs}
         return self.__dict__
