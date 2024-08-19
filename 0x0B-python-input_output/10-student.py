f9eebac6ea56151adf8c1f6edc846cba7f35cd1f
Amir Murad <amirmurad696@gmail.com>
2024-08-19 16:59:15 +0300

    Returned to the first version

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,12 +11,22 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instan
         ce"""
 
-        if isinstance(attrs, list) and all(isinstance(attr, str) for
-                                           attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasat
-                    tr(self, key)}
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
