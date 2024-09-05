0d0b29fefdebff23794aaa2d92ce747d219977c8
Amir Murad <amirmurad696@gmail.com>
2024-09-05 20:31:42 +0300

    Added a property decorator

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,17 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square (both width and height)."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Returns the str() representation of a Square."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
